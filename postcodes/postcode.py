7f829cd559d7d431d51fd7d00c457553abe68914
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-01-10 20:16:20 +0000

    Define postcode components

--- postcodes/postcode.py
+++ postcodes/postcode.py
@@ -1,4 +1,16 @@
 from dataclasses import dataclass
+from enum import Enum, auto
+
+
+class Component(Enum):
+    COMPLETE = auto()
+    OUTWARD = auto()
+    AREA = auto()
+    DISTRICT = auto()
+    SUBDISTRICT = auto()
+    INWARD = auto()
+    SECTOR = auto()
+    UNIT = auto()
 
 
 @dataclass
