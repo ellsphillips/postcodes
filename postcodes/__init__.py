70ce048f122f69d8d521a552f7e6439fdda75521
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-01-10 20:15:40 +0000

    Create postcode structure

---
+++ postcodes/__init__.py
@@ -0,0 +1,6 @@
+"""
+UK postcodes, created by Royal Mail, are composed of 5 to 7 alphanumeric characters.
+A full postcode designates an area with multiple addresses or a single delivery point.
+"""
+
+from .postcode import Postcode
