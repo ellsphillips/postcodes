fbed1bac672fe02a375ae7e93980acba56f2d390
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-01-11 15:42:31 +0000

    Make pattern LUTs accessible at module level

--- postcodes/__init__.py
+++ postcodes/__init__.py
@@ -3,4 +3,5 @@ UK postcodes, created by Royal Mail, are composed of 5 to 7 alphanumeric charact
 A full postcode designates an area with multiple addresses or a single delivery point.
 """
 
+from .patterns import KEY_PATTERNS, UK_PATTERNS
 from .postcode import Postcode
