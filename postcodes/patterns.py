dfe60726b398bb632eaddf0f90874a922e238d39
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-01-14 11:38:22 +0000

    Add GiroBank support

--- postcodes/patterns.py
+++ postcodes/patterns.py
@@ -14,4 +14,5 @@ UK_PATTERNS = {
     "british_overseas_territories": r"^((ASCN|STHL|TDCU|BBND|[BFS]IQQ|GX\d{2}|PCRN|TKCA) ?\d[A-Z]{2}|(KY\d|MSR|VG|AI)[ -]?\d{4}|(BFPO|[A-Z]{2}) ?\d{2}|GE ?CX)$",  # pylint: disable=line-too-long
     "british_forces_post_office": r"^BFPO ?\d{1,4}$",
     "santa": r"^SAN ?TA1$",
+    "girobank": r"^(GIR)\s*(0AA)",
 }
